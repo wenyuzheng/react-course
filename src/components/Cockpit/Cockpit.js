d0aa25b3a4552430f424d6cdf02d3010c1963233
Wenyu Zheng <zhengwy229@gmail.com>
2020-06-24 08:15:21 +0100

    appTitle

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -18,7 +18,7 @@ const cockpit = (props) => {
     
     return (
         <div className={classes.Cockpit}>
-            <h1>hello</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(" ")}> this is working</p>
             <button
                 className={btnClass}
